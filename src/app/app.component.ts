492fd461cfe6e92c83308dfc1536ff0fb973b1c9
Joe Cserko <j.cserko@gmail.com>
2021-11-24 19:04:41 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var System: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dynamic-component-cherryapp';
+
+  constructor() {
+    const t = System.import('https://gist.githubusercontent.com/dianadujing/a7bbbf191349182e1d459286dba0282f/raw/c23281f8c5fabb10ab9d144489316919e4233d11/app.module.ts');
+    t.then( (module: any) => console.log(module))
+  }
 }
